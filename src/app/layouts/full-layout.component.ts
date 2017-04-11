66a525705c335c6f7ee455a6d282a365e4936825
robertodebarba <roberto.debarba@gmail.com>
2017-04-10 21:10:32 -0300

    Remoçao de código não utilizado

--- src/app/layouts/full-layout.component.ts
+++ src/app/layouts/full-layout.component.ts
@@ -1,25 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './full-layout.component.html'
 })
-export class FullLayoutComponent implements OnInit {
+export class FullLayoutComponent {
 
   constructor() { }
 
-  public disabled:boolean = false;
-  public status:{isopen:boolean} = {isopen: false};
-
-  public toggled(open:boolean):void {
-    console.log('Dropdown is now: ', open);
-  }
-
-  public toggleDropdown($event:MouseEvent):void {
-    $event.preventDefault();
-    $event.stopPropagation();
-    this.status.isopen = !this.status.isopen;
-  }
-
-  ngOnInit(): void {}
 }
