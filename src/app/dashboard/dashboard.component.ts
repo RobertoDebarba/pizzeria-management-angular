435a24a21a705f60e96bba37cdf9e0aa5437d366
robertodebarba <roberto.debarba@gmail.com>
2017-04-12 19:32:30 -0300

    Alterado para mostrar tela ativa

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+
+interface Page {
+    title:string,
+    route:string
+}
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent {
 
-  constructor() { }
+    public pages:Page[] = [
+        {title: 'Pedidos', route: 'order'},
+        {title: 'Cliente', route: 'client'},
+        {title: 'Produto', route: 'product'},
+    ];
+
+    constructor(private router:Router) {
+    }
+
+    public isActivePage(page:Page):boolean {
+        return this.router.url.indexOf(page.route) > 0
+    }
 
 }
