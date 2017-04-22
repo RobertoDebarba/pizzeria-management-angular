f0ebae16e7a01e8a9cbe5503e2a5dff1050a997a
robertodebarba <roberto.debarba@gmail.com>
2017-04-22 17:22:27 -0300

    Implementado serviço de CLientes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
 
-import HeroRouter from './routes/HeroRouter';
+import ClientRouter from './routes/client-router';
 
 class App {
 
@@ -32,7 +32,7 @@ class App {
             });
         });
         this.express.use('/', router);
-        this.express.use('/api/v1/heroes', HeroRouter);
+        this.express.use('/api/client', ClientRouter);
     }
 
 }
---
+++ backend/src/routes/client-router.ts
@@ -0,0 +1,86 @@
+import {Router, Request, Response} from "express";
+import * as fs from "fs";
+import * as path from "path";
+
+const dataFilePath:string = '../data/client';
+
+interface Client {
+    name:string,
+    cpf:number,
+    phone1:number,
+    phone2:number,
+    address:{
+        place:string,
+        city:string,
+        zipCode:number,
+        number:number,
+        neighborhood:string,
+        info:string
+    }
+}
+
+export class ClientRouter {
+
+    router:Router;
+
+    constructor() {
+        this.router = Router();
+        this.init();
+    }
+
+    public init() {
+        this.router.get('/', this.getAll);
+        this.router.get('/:cpf', this.getOne);
+        this.router.post('/', this.save);
+    }
+
+    private getAll(request:Request, response:Response) {
+        let clients:Client[] = require(dataFilePath);
+        response.send(clients);
+    }
+
+    private getOne(request:Request, response:Response) {
+        let clients:Client[] = require(dataFilePath);
+
+        let query:number = parseInt(request.params.cpf);
+        let client:Client = clients.find((client:Client) => client.cpf === query);
+        if (client) {
+            response.status(200)
+                .send({
+                    message: 'Success',
+                    status: response.status,
+                    client
+                });
+        }
+        else {
+            response.status(404)
+                .send({
+                    message: 'No client found with the given cpf.',
+                    status: response.status
+                });
+        }
+    }
+
+    private save(request:Request, response:Response) {
+        let clients:Client[] = require(dataFilePath);
+
+        let client:Client = request.body;
+
+        clients.push(client);
+        fs.writeFile(path.join(__dirname, dataFilePath), JSON.stringify(clients), 'UTF-8', () => {
+            clients = require(dataFilePath);
+            response.status(200)
+                .send({
+                    message: 'Success',
+                    status: response.status,
+                    client
+                });
+        })
+    }
+
+}
+
+const clientRouter = new ClientRouter();
+clientRouter.init();
+
+export default clientRouter.router;
