a5b407f94677d9e1d08dfa1479f08a6954afbb89
BENNER\matheus.pereira <matheus.pereira@benner.com.br>
2017-04-25 12:48:17 -0300

    grid Products

--- frontend/src/app/product/product.component.ts
+++ frontend/src/app/product/product.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { ProductService, Product } from '../provider/product.services'
 
 @Component({
-  templateUrl: 'product.component.html'
+  templateUrl: 'product.component.html',
+  providers: [ProductService]
 })
 export class ProductComponent {
+  AllProducts : Product[];
 
   public name: string;
   public price: number;
 
-  constructor( ) { }
+  constructor(private productService: ProductService) {
+    this.AtualizaProdutos();
+  }
+
+  private AtualizaProdutos(){
+    this.productService.getProducts().subscribe(p => this.AllProducts = p);
+  }
 
 }
